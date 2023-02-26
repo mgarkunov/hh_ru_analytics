67db50596fe21c695a4b3af8433435532f589ee4
Michail  Garkunov <78558081+mgarkunov@users.noreply.github.com>
2023-02-26 15:10:36 +0300

    Create hh_api_load.py

---
+++ python/hh_api_load.py
@@ -0,0 +1,21 @@
+#%%
+import datetime as dt
+import pandas as pd
+import requests
+
+
+def json_to_flatdf(response):
+  """
+  Функция преобразования json в плоскую таблицу
+  """
+  pass
+
+def load_list_vacancies(start_date:dt.date, end_date:dt.date, pfof_id ) => dict:
+  """
+  Функция для загрузки списка вакансий
+  НАдо будет сделать цикл, который будет обходит от 
+  """
+  pass
+  
+#%%
+type([])
