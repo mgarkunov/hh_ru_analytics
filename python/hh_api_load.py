2084c1fe0c69a12f95af50430a3d57858515bdec
Michail  Garkunov <78558081+mgarkunov@users.noreply.github.com>
2023-02-26 17:33:54 +0300

    Update hh_api_load.py

--- python/hh_api_load.py
+++ python/hh_api_load.py
@@ -3,6 +3,16 @@ import datetime as dt
 import pandas as pd
 import requests
 
+### --- Конфигруация логгирования / --- ###
+import logging, sys
+logger = logging.getLogger('debug')
+handler_console = logging.StreamHandler(sys.stdout)
+handler_console.setFormatter(logging.Formatter(fmt='[%(asctime)s: %(levelname)s] %(message)s', datefmt='%Y-%m-%d %T'))
+logger.handlers = []
+logger.addHandler(handler_console)
+logger.setLevel(logging.DEBUG)
+### --- / Конфигруация логгирования --- ###
+
 
 def json_to_flatdf(response):
   """
@@ -10,11 +20,22 @@ def json_to_flatdf(response):
   """
   pass
 
-def load_list_vacancies(start_date:dt.date, end_date:dt.date, pfof_id:list = [], search:str = ""):
+def load_list_vacancies(start_date:dt.date, end_date:dt.date, pкof_id:list = [], search:str = ""):
   """
   Функция для загрузки списка вакансий
-  НАдо будет сделать цикл, который будет обходит от 
+  НАдо будет сделать цикл, который будет обходит по дням и часам.
+  pкof_id - список для id профессий
+  search - поиковая фраза
   """
   pass
+
+def load_detail_vacancy(vac_list:list):
+  """
+  vac_list - список id с вакансиями из списка и далее нужно будет получить детально, 
+  т.е. нужно сделать алгоритм обхода списка
+  """
+
+# Алогоритм работы скрипта
+# 1. Загружаем 
 
 
