c114e4d49f20ccb0168a65c776d0b72fcfeea966
Sergey-Losev <srglosev@gmail.com>
2023-03-19 03:17:17 +0100

    updated script

--- python/main_hh.py
+++ python/main_hh.py
@@ -6,6 +6,7 @@ import datetime as dt
 import yaml
 import sqlalchemy as sa
 import psycopg2
+from flatten_json import flatten
 
 
 ### --- Конфигруация логгирования / --- ###
@@ -19,18 +20,28 @@ logger.setLevel(logging.DEBUG)
 ### --- / Конфигруация логгирования --- ###
 
 
-cfg = yaml.safe_load(open('cfg.yaml', 'r'))
+cfg = yaml.safe_load(open('/home/serge/pet_project/cfg.yaml', 'r'))
 psql = sa.create_engine(f"postgresql://{cfg['psql']['user']}:{cfg['psql']['pwd']}@{cfg['psql']['host']}:{cfg['psql']['port']}/{cfg['psql']['dbname']}")
 
 
 url_HH = 'https://api.hh.ru/vacancies'
-pкof_id = "(Руководитель отдела аналитики) OR (BI-аналитик, аналитик данных) OR (Аналитик) OR \
-    (Бизнес-аналитик) OR (Продуктовый аналитик) OR (Системный аналитик) OR (Маркетолог-аналитик) \
-        OR (Финансовый аналитик, инвестиционный аналитик) OR (Дата-сайентист)"
-search = "(Data Engineer OR Data Scientist OR Data Analyst OR data science engineer OR Аналитик данных \
-    OR Бизнес аналитик OR финансовый аналитик OR системный аналитик OR системная аналитика OR продуктовый аналитик \
-        OR дата инженер OR инженер данных OR devops инженер OR датасайнтист OR\
-            (Аналитик AND (систем OR продукт OR бизнес OR данн OR финанс)))"
+pкof_id = [
+    10, # Аналитик
+    40, # Другое
+    134, # Финансовый аналитик, инвестиционный аналитик
+    148, # Системный аналитик
+    150, # Бизнес-аналитик
+    156, # BI-аналитик, аналитик данных
+    157, # Руководитель отдела аналитики
+    160, # DevOps-инженер
+    163, # Маркетолог-аналитик
+    164, # Продуктовый аналитик
+    165, # Дата-сайентист
+    ]
+search = "NAME:((Data Engineer) OR (Data Scientist) OR (Data Analyst) OR (data science engineer) \
+    OR (Аналитик данных) OR (Бизнес аналитик) OR (финансовый аналитик) OR (системный аналитик) \
+        OR (системная аналитика) OR (продуктовый аналитик) OR (дата инженер) OR (инженер данных) \
+            OR (devops инженер) OR (датасайнтист) OR (Аналитик AND (систем* OR продукт* OR бизнес* OR данн* OR финанс*)))"
 
 
 # --- Инициализация базы / -- #
@@ -50,14 +61,6 @@ else:
     raise ValueError('Неправильное название базы')
 # --- / Инициализация базы -- #
 #%%
-
-"""
-В скрипте пропущен тонкий момент с пагинацией, обязательно добавлю!!!
-TO-DO:
-1. Учесть пагинацию
-2. Переписать get_raw_data, сделать понятнее
-3. Разобраться с send_data, чтобы грузить весь массив данных
-"""
 
 def json_to_flatdf(response):
     """
@@ -88,28 +91,30 @@ def json_to_flatdf(response):
     return pd.DataFrame.from_dict(new_dict, orient='index').transpose()
 
 
-def load_list_vacancies(start_date:dt.date, end_date:dt.date, pкof_id:str = "", search:str = ""):
+def load_list_vacancies(start_date:dt.date, end_date:dt.date, pкof_id:list = [], search:str = ""):
     """
     Функция для загрузки списка вакансий
     НАдо будет сделать цикл, который будет обходит по дням и часам.
     pкof_id - список для id профессий
     search - поиковая фраза
     """
-    vac_request = f'NAME: {search} OR SPECIALIZATION: {pкof_id}'
     df = pd.DataFrame()
-    ids = []
     while start_date < end_date:
         query_params = {
-            'text': vac_request, 
+            'text': search, 
             'area': 113, 
             'per_page': 100,
             'date_from': start_date.strftime("%Y-%m-%dT%H:%M:%S"),
             'date_to': (start_date + dt.timedelta(hours=1, seconds=-1)).strftime("%Y-%m-%dT%H:%M:%S")
-        }
-        start_date = start_date + dt.timedelta(hours=1)
+            }
         response = requests.get(url=url_HH, params=query_params).json()
-        df = pd.concat([df, pd.DataFrame(response['items'])])
-    ids = df.id.to_list()
+        print(response['found'])
+        for page in range(response['pages']):
+            query_params['page'] = page
+            response = requests.get(url=url_HH, params=query_params).json()
+            df = pd.concat([df, pd.DataFrame(response['items'])])
+        start_date = start_date + dt.timedelta(hours=1)
+    ids = df['id'].to_list()
     return ids
 
 
@@ -120,9 +125,13 @@ def get_vacancies(id_list):
     """
     vac_df = pd.DataFrame()
     for vac_id in id_list:
+        print(vac_id)
         vac_response = requests.get(url=f"{url_HH}/{vac_id}")
         # Где функция пробразования JSON в плоскую таблицу - починил
+        flat_df = flatten(vac_response)
         vac_df = pd.concat([vac_df, json_to_flatdf(vac_response)])
+    
+    vac_df['tech_change_date'] = dt.datetime.timestamp(dt.datetime.now())
     return vac_df
 
 
@@ -133,9 +142,6 @@ def send_data(df):
     Пока что её так и не поборол
     В итоге оставил 11 полей, прописал их типы и таки положил в базу.
     """
-    cols_list = ['id', 'premium', 'name', 'description', 'key_skills', 'archived', 'specializations', 'professional_roles',
-                 'published_at', 'created_at', 'initial_created_at']
-    df = df.loc[:, cols_list]
     df.to_sql(
         name='list_of_vacancies',
         con=psql,
@@ -143,17 +149,104 @@ def send_data(df):
         if_exists='replace',
         index=False,
         dtype={
-            'id': sa.types.Integer,
-            'premium': sa.types.Boolean,
-            'name': sa.types.String,
+            'id': sa.types.Integer, 
+            'premium': sa.types.Boolean, 
+            'billing_type_id': sa.types.String, 
+            'billing_type_name': sa.types.String, 
+            'relations': sa.types.String,
+            'name': sa.types.String, 
+            'insider_interview': sa.types.Boolean, 
+            'response_letter_required': sa.types.Boolean, 
+            'area_id': sa.types.Integer,
+            'area_name': sa.types.String, 
+            'area_url': sa.types.String, 
+            'salary': sa.types.Integer, 
+            'type_id': sa.types.String, 
+            'type_name': sa.types.String,
+            'address_city': sa.types.String,
+            'address_street': sa.types.String,
+            'address_building': sa.types.String,
+            'address_lat': sa.types.Float,
+            'address_lng': sa.types.Float,
+            'address_description': sa.types.String,
+            'address_raw': sa.types.String,
+            'address_metro_station_name': sa.types.String,
+            'address_metro_line_name': sa.types.String,
+            'address_metro_station_id': sa.types.String,
+            'address_metro_line_id': sa.types.String,
+            'address_metro_lat': sa.types.Float,
+            'address_metro_lng': sa.types.Float,
+            'address_metro_stations': sa.types.String,
+            'allow_messages': sa.types.Boolean,
+            'experience_id': sa.types.String,
+            'experience_name': sa.types.String,
+            'schedule_id': sa.types.String,
+            'schedule_name': sa.types.String,
+            'employment_id': sa.types.String, 
+            'employment_name': sa.types.String, 
+            'department': sa.types.String,
+            'contacts': sa.types.String,
             'description': sa.types.String,
-            'key_skills': sa.types.JSON,
+            'vacancy_constructor_template': sa.types.String,
+            'key_skills': sa.types.String,
+            'accept_handicapped': sa.types.Boolean,
+            'accept_kids': sa.types.Boolean,
             'archived': sa.types.Boolean,
-            'specializations': sa.types.JSON,
-            'professional_roles': sa.types.JSON,
+            'response_url': sa.types.String,
+            'specializations': sa.types.String,
+            'professional_roles': sa.types.String, 
+            'code': sa.types.String,
+            'hidden': sa.types.Boolean,
+            'quick_responses_allowed': sa.types.Boolean,
+            'driver_license_types': sa.types.String,
+            'accept_incomplete_resumes': sa.types.Boolean, 
+            'employer_id': sa.types.Integer, 
+            'employer_name': sa.types.String,
+            'employer_url': sa.types.String,
+            'employer_alternate_url': sa.types.String,
+            'employer_logo_urls_90': sa.types.String,
+            'employer_logo_urls_240': sa.types.String,
+            'employer_logo_urls_original': sa.types.String,
+            'employer_vacancies_url': sa.types.String, 
+            'employer_trusted': sa.types.String, 
             'published_at': sa.types.DATE,
-            'created_at': sa.types.DATE,
-            'initial_created_at': sa.types.DATE
+            'created_at': sa.types.DATE, 
+            'initial_created_at': sa.types.DATE, 
+            'negotiations_url': sa.types.String,
+            'suitable_resumes_url': sa.types.String, 
+            'apply_alternate_url': sa.types.String, 
+            'has_test': sa.types.Boolean, 
+            'test': sa.types.Boolean,
+            'alternate_url': sa.types.String, 
+            'working_days': sa.types.String,
+            'working_time_intervals': sa.types.String,
+            'working_time_modes': sa.types.String, 
+            'accept_temporary': sa.types.Boolean,
+            'languages': sa.types.String, 
+            'address': sa.types.String,
+            'salary_from': sa.types.Integer,
+            'salary_to': sa.types.Integer, 
+            'salary_currency': sa.types.String, 
+            'salary_gross': sa.types.Boolean,
+            'address_metro': sa.types.String, 
+            'department_id': sa.types.String,
+            'department_name': sa.types.String,
+            'insider_interview_id': sa.types.String, 
+            'insider_interview_url': sa.types.String, 
+            'employer_logo_urls': sa.types.String,
+            'test_required': sa.types.Boolean, 
+            'vacancy_constructor_template_id': sa.types.String,
+            'vacancy_constructor_template_name': sa.types.String,
+            'vacancy_constructor_template_top_picture_height': sa.types.String,
+            'vacancy_constructor_template_top_picture_width': sa.types.String,
+            'vacancy_constructor_template_top_picture_path': sa.types.String,
+            'vacancy_constructor_template_top_picture_blurred_path': sa.types.String,
+            'vacancy_constructor_template_bottom_picture': sa.types.String,
+            'vacancy_constructor_template_bottom_picture_height': sa.types.String,
+            'vacancy_constructor_template_bottom_picture_width': sa.types.String,
+            'vacancy_constructor_template_bottom_picture_path': sa.types.String,
+            'vacancy_constructor_template_bottom_picture_blurred_path': sa.types.String,
+            'tech_change_date': sa.types.TIMESTAMP
         }
     )
 
@@ -161,20 +254,124 @@ def send_data(df):
 #%%
 def main():
 
-    date_from = dt.datetime.combine(dt.date.today(), dt.datetime.min.time()) - dt.timedelta(days=2)
-    date_to = dt.datetime.combine(dt.date.today(), dt.datetime.min.time()) - dt.timedelta(days=1)
+    start_date = dt.datetime.today() - dt.timedelta(hours=26)
+    end_date = dt.datetime.today() - dt.timedelta(days=1)
 
     
     # Получаем список подходящих под наши критерии вакансий
-    id_list = load_list_vacancies(date_from, date_to, pкof_id, search)
+    id_list = load_list_vacancies(start_date, end_date, pкof_id, search)
+    print(len(id_list))
     # Создаем датафрейм с детальным описанием
     df_with_vac = get_vacancies(id_list)
+    print(len(df_with_vac.columns))
+    print(df_with_vac.columns)
     # Загружаем данные в базу
     send_data(df_with_vac)
-    
 
-    
+
 if __name__ == "__main__":
     main()
 
+# %%
+id_list = {
+    'id': sa.types.Integer, 
+    'premium': sa.types.Boolean, 
+    'billing_type_id': sa.types.String, 
+    'billing_type_name': sa.types.String, 
+    'relations': sa.types.String,
+    'name': sa.types.String, 
+    'insider_interview': sa.types.Boolean, 
+    'response_letter_required': sa.types.Boolean, 
+    'area_id': sa.types.Integer,
+    'area_name': sa.types.String, 
+    'area_url': sa.types.String, 
+    'salary': sa.types.Integer, 
+    'type_id': sa.types.String, 
+    'type_name': sa.types.String,
+    'address_city': sa.types.String,
+    'address_street': sa.types.String,
+    'address_building': sa.types.String,
+    'address_lat': sa.types.Float,
+    'address_lng': sa.types.Float,
+    'address_description': sa.types.String,
+    'address_raw': sa.types.String,
+    'address_metro_station_name': sa.types.String,
+    'address_metro_line_name': sa.types.String,
+    'address_metro_station_id': sa.types.String,
+    'address_metro_line_id': sa.types.String,
+    'address_metro_lat': sa.types.Float,
+    'address_metro_lng': sa.types.Float,
+    'address_metro_stations': sa.types.String,
+    'allow_messages': sa.types.Boolean,
+    'experience_id': sa.types.String,
+    'experience_name': sa.types.String,
+    'schedule_id': sa.types.String,
+    'schedule_name': sa.types.String,
+    'employment_id': sa.types.String, 
+    'employment_name': sa.types.String, 
+    'department': sa.types.String,
+    'contacts': sa.types.String,
+    'description': sa.types.String,
+    'vacancy_constructor_template': sa.types.String,
+    'key_skills': sa.types.String,
+    'accept_handicapped': sa.types.Boolean,
+    'accept_kids': sa.types.Boolean,
+    'archived': sa.types.Boolean,
+    'response_url': sa.types.String,
+    'specializations': sa.types.String,
+    'professional_roles': sa.types.String, 
+    'code': sa.types.String,
+    'hidden': sa.types.Boolean,
+    'quick_responses_allowed': sa.types.Boolean,
+    'driver_license_types': sa.types.String,
+    'accept_incomplete_resumes': sa.types.Boolean, 
+    'employer_id': sa.types.Integer, 
+    'employer_name': sa.types.String,
+    'employer_url': sa.types.String,
+    'employer_alternate_url': sa.types.String,
+    'employer_logo_urls_90': sa.types.String,
+    'employer_logo_urls_240': sa.types.String,
+    'employer_logo_urls_original': sa.types.String,
+    'employer_vacancies_url': sa.types.String, 
+    'employer_trusted': sa.types.String, 
+    'published_at': sa.types.DATE,
+    'created_at': sa.types.DATE, 
+    'initial_created_at': sa.types.DATE, 
+    'negotiations_url': sa.types.String,
+    'suitable_resumes_url': sa.types.String, 
+    'apply_alternate_url': sa.types.String, 
+    'has_test': sa.types.Boolean, 
+    'test': sa.types.Boolean,
+    'alternate_url': sa.types.String, 
+    'working_days': sa.types.String,
+    'working_time_intervals': sa.types.String,
+    'working_time_modes': sa.types.String, 
+    'accept_temporary': sa.types.Boolean,
+    'languages': sa.types.String, 
+    'address': sa.types.String,
+    'salary_from': sa.types.Integer,
+    'salary_to': sa.types.Integer, 
+    'salary_currency': sa.types.String, 
+    'salary_gross': sa.types.Boolean,
+    'address_metro': sa.types.String, 
+    'department_id': sa.types.String,
+    'department_name': sa.types.String,
+    'insider_interview_id': sa.types.String, 
+    'insider_interview_url': sa.types.String, 
+    'employer_logo_urls': sa.types.String,
+    'test_required': sa.types.Boolean, 
+    'vacancy_constructor_template_id': sa.types.String,
+    'vacancy_constructor_template_name': sa.types.String,
+    'vacancy_constructor_template_top_picture_height': sa.types.String,
+    'vacancy_constructor_template_top_picture_width': sa.types.String,
+    'vacancy_constructor_template_top_picture_path': sa.types.String,
+    'vacancy_constructor_template_top_picture_blurred_path': sa.types.String,
+    'vacancy_constructor_template_bottom_picture': sa.types.String,
+    'vacancy_constructor_template_bottom_picture_height': sa.types.String,
+    'vacancy_constructor_template_bottom_picture_width': sa.types.String,
+    'vacancy_constructor_template_bottom_picture_path': sa.types.String,
+    'vacancy_constructor_template_bottom_picture_blurred_path': sa.types.String,
+    'tech_change_date': sa.types.TIMESTAMP
+}
+print(*id_list, sep=',\n')
 # %%
