ea6c42f61c0fe5b8124f6ca2770ddbcbc6169b42
Sergey-Losev <srglosev@gmail.com>
2023-03-25 11:39:02 +0100

    last update 2023-03-25

--- python/main_hh.py
+++ python/main_hh.py
@@ -7,6 +7,8 @@ import yaml
 import sqlalchemy as sa
 import psycopg2
 from flatten_json import flatten
+import json
+import os
 
 
 ### --- Конфигруация логгирования / --- ###
@@ -27,7 +29,7 @@ psql = sa.create_engine(f"postgresql://{cfg['psql']['user']}:{cfg['psql']['pwd']
 url_HH = 'https://api.hh.ru/vacancies'
 pкof_id = [
     10, # Аналитик
-    40, # Другое
+    # 40, # Другое - пока что спрятал в коммент, потому что в этот раздел поподает вообще всё, что не подошло к другим специализациям
     134, # Финансовый аналитик, инвестиционный аналитик
     148, # Системный аналитик
     150, # Бизнес-аналитик
@@ -64,39 +66,24 @@ else:
 
 def json_to_flatdf(response):
     """
-    Функция преобразования json в плоскую таблицу
+    Функция преобразования в плоскую таблицу
     """
-    new_dict = {}
-    # проходим по словарю и проверяем наличие словарей в значениях, если такие присутствуют, то раскладываем
-    for key, value in response.items():
-        if type(value) == dict:
-            for item in value.keys():
-                if type(value[item]) == dict:
-                    for item_2 in value[item].keys():
-                        new_dict[f'{key}_{item}_{item_2}'] = value[item][item_2]
-                else:
-                    new_dict[f'{key}_{item}'] = value[item]
-    # далее исследуем значения, где могут быть списки со словарями, укладываем их просто в список без словарей
-        elif key == 'professional_roles':
-            sl = []
-            for item in value:
-                try:
-                    sl.append(item['name'])
-                except:
-                    pass
-            new_dict[key] = sl
-        elif key == 'description':
-            new_dict[key] = re.sub(r'<.*?>', '', value)
-        elif key == 'key_skills':
-            help_list = []
-            for item in value:
-                help_list.append(item['name'])
-            new_dict[key] = help_list
+    out = {}
+
+    def flatten(x, name=''):
+        if type(x) is dict:
+            for a in x:
+                flatten(x[a], name + a + '_')
+        elif type(x) is list:
+            i = 0
+            for a in x:
+                flatten(a, name + str(i) + '_')
+                i += 1
         else:
-            new_dict[key] = value
-    new_dict = flatten(new_dict, root_keys_to_ignore={'professional_roles', 'description', 'key_skills'})
-    final_df = pd.DataFrame([new_dict])
-    return final_df
+            out[name[:-1]] = x
+
+    flatten(response)
+    return pd.DataFrame([out])
 
 
 def load_list_vacancies(start_date:dt.date, end_date:dt.date, pкof_id:list = [], search:str = ""):
@@ -116,12 +103,17 @@ def load_list_vacancies(start_date:dt.date, end_date:dt.date, pкof_id:list = []
             'date_from': start_date.strftime("%Y-%m-%dT%H:%M:%S"),
             'date_to': (start_date + dt.timedelta(hours=1, seconds=-1)).strftime("%Y-%m-%dT%H:%M:%S")
             }
-        response = requests.get(url=url_HH, params=query_params).json()
-        # проходим по страницам
-        for page in range(response['pages']):
+        page = 0
+        while True:
             query_params['page'] = page
             response = requests.get(url=url_HH, params=query_params).json()
-            df = pd.concat([df, pd.DataFrame(response['items'])])
+            print(response['found'])
+            df = pd.concat([df, pd.DataFrame(response['items'], index=None)])
+            if page < (response['pages'] - 1):
+                page += 1
+                print(page, response['pages'], end='\r')
+            else:
+                break
         # далее исследуем запрос по профессиональным ролям
         query_params = {
             'professional_role': pкof_id, 
@@ -130,40 +122,49 @@ def load_list_vacancies(start_date:dt.date, end_date:dt.date, pкof_id:list = []
             'date_from': start_date.strftime("%Y-%m-%dT%H:%M:%S"),
             'date_to': (start_date + dt.timedelta(hours=1, seconds=-1)).strftime("%Y-%m-%dT%H:%M:%S")
             }
-        response = requests.get(url=url_HH, params=query_params).json()
-        # проходим по страницам
-        for page in range(response['pages']):
+        page = 0
+        while True:
             query_params['page'] = page
             response = requests.get(url=url_HH, params=query_params).json()
-            df = pd.concat([df, pd.DataFrame(response['items'])])
+            print(response['found'])
+            df = pd.concat([df, pd.DataFrame(response['items'], index=None)])
+            if page < (response['pages'] - 1):
+                page += 1
+                print(page, response['pages'], end='\r')
+            else:
+                break
         start_date = start_date + dt.timedelta(hours=1)
+        os.system("clear")
     # в конце берем столбец id из получившегося датафрейма и возвращаем в качестве списка
     ids = set(df['id'].to_list())
+    print(len(ids))
     return ids
-
 
 
 def get_vacancies(id_list):
     """
     Функция для создания массива детального описания всех вакансий по заданным критериям
     """
-    vac_df = pd.DataFrame()
+    vacs_list = []
     for vac_id in id_list:
         print(vac_id)
         vac_response = requests.get(url=f"{url_HH}/{vac_id}").json()
-        # Где функция пробразования JSON в плоскую таблицу - починил
-
-        vac_df = pd.concat([vac_df, json_to_flatdf(vac_response)])
+        vac_response['description'] = re.sub(r'<.*?>', '', vac_response['description'])
+        del vac_response['branded_description']
+        vacs_list.append(vac_response)
     
-    vac_df['tech_change_date'] = dt.datetime.timestamp(dt.datetime.now())
+    with open(f"{dt.date.today()}_test.json", "w") as f:
+        json.dump(vacs_list, f, indent=4, ensure_ascii=False)
+    vac_df = pd.DataFrame(vacs_list)
+    vac_df['tech_change_date'] = dt.datetime.now()
+    os.system("clear")
+    
     return vac_df
 
 
 def send_data(df):
     """
-    В 3 часа ночи HH начал выдавать ошибку captcha_required.
-    В итоге - когда небольшое кол-во полей (меньше 70) - скрипт укладывает вакансии в базу, когда 
-    Когда больше - выдает ошибку, так и не смог понять от чего это зависит. Не успел...
+    Функция записи датафрейма с набором вакансий в базу данных
     """
     df.to_sql(
         name='vacancies_list',
@@ -174,64 +175,36 @@ def send_data(df):
         dtype={
             'id': sa.types.Integer, 
             'premium': sa.types.Boolean, 
-            'billing_type_id': sa.types.String, 
-            'billing_type_name': sa.types.String, 
+            'billing_type': sa.types.JSON,
             'relations': sa.types.String,
             'name': sa.types.String, 
             'insider_interview': sa.types.Boolean, 
             'response_letter_required': sa.types.Boolean, 
-            'area_id': sa.types.Integer,
-            'area_name': sa.types.String, 
-            'area_url': sa.types.String, 
-            'salary': sa.types.Integer, 
-            'type_id': sa.types.String, 
-            'type_name': sa.types.String,
-            'address_city': sa.types.String,
-            'address_street': sa.types.String,
-            'address_building': sa.types.String,
-            'address_lat': sa.types.Float,
-            'address_lng': sa.types.Float,
-            'address_description': sa.types.String,
-            'address_raw': sa.types.String,
-            'address_metro_station_name': sa.types.String,
-            'address_metro_line_name': sa.types.String,
-            'address_metro_station_id': sa.types.String,
-            'address_metro_line_id': sa.types.String,
-            'address_metro_lat': sa.types.Float,
-            'address_metro_lng': sa.types.Float,
-            'address_metro_stations': sa.types.String,
+            'area': sa.types.JSON,
+            'salary': sa.types.JSON, 
+            'type': sa.types.JSON,
+            'address': sa.types.JSON,
             'allow_messages': sa.types.Boolean,
-            'experience_id': sa.types.String,
-            'experience_name': sa.types.String,
-            'schedule_id': sa.types.String,
-            'schedule_name': sa.types.String,
-            'employment_id': sa.types.String, 
-            'employment_name': sa.types.String, 
-            'department': sa.types.String,
+            'experience': sa.types.JSON,
+            'schedule': sa.types.JSON,
+            'employment': sa.types.String, 
+            'department': sa.types.JSON,
             'contacts': sa.types.String,
-            'description': sa.types.String,
+            'description': sa.types.Text,
             'vacancy_constructor_template': sa.types.String,
-            'key_skills': sa.types.String,
+            'key_skills': sa.types.JSON,
             'accept_handicapped': sa.types.Boolean,
             'accept_kids': sa.types.Boolean,
             'archived': sa.types.Boolean,
             'response_url': sa.types.String,
-            'specializations': sa.types.String,
-            'professional_roles': sa.types.String, 
+            'specializations': sa.types.JSON,
+            'professional_roles': sa.types.JSON, 
             'code': sa.types.String,
             'hidden': sa.types.Boolean,
             'quick_responses_allowed': sa.types.Boolean,
-            'driver_license_types': sa.types.String,
+            'driver_license_types': sa.types.JSON,
             'accept_incomplete_resumes': sa.types.Boolean, 
-            'employer_id': sa.types.Integer, 
-            'employer_name': sa.types.String,
-            'employer_url': sa.types.String,
-            'employer_alternate_url': sa.types.String,
-            'employer_logo_urls_90': sa.types.String,
-            'employer_logo_urls_240': sa.types.String,
-            'employer_logo_urls_original': sa.types.String,
-            'employer_vacancies_url': sa.types.String, 
-            'employer_trusted': sa.types.String, 
+            'employer': sa.types.JSON,
             'published_at': sa.types.DATE,
             'created_at': sa.types.DATE, 
             'initial_created_at': sa.types.DATE, 
@@ -241,44 +214,21 @@ def send_data(df):
             'has_test': sa.types.Boolean, 
             'test': sa.types.Boolean,
             'alternate_url': sa.types.String, 
-            'working_days': sa.types.String,
-            'working_time_intervals': sa.types.String,
+            'working_days': sa.types.JSON,
+            'working_time_intervals': sa.types.JSON,
             'working_time_modes': sa.types.String, 
             'accept_temporary': sa.types.Boolean,
-            'languages': sa.types.String, 
-            'address': sa.types.String,
-            'salary_from': sa.types.Integer,
-            'salary_to': sa.types.Integer, 
-            'salary_currency': sa.types.String, 
-            'salary_gross': sa.types.Boolean,
-            'address_metro': sa.types.String, 
-            'department_id': sa.types.String,
-            'department_name': sa.types.String,
-            'insider_interview_id': sa.types.String, 
-            'insider_interview_url': sa.types.String, 
-            'employer_logo_urls': sa.types.String,
-            'test_required': sa.types.Boolean, 
-            'vacancy_constructor_template_id': sa.types.String,
-            'vacancy_constructor_template_name': sa.types.String,
-            'vacancy_constructor_template_top_picture_height': sa.types.String,
-            'vacancy_constructor_template_top_picture_width': sa.types.String,
-            'vacancy_constructor_template_top_picture_path': sa.types.String,
-            'vacancy_constructor_template_top_picture_blurred_path': sa.types.String,
-            'vacancy_constructor_template_bottom_picture': sa.types.String,
-            'vacancy_constructor_template_bottom_picture_height': sa.types.String,
-            'vacancy_constructor_template_bottom_picture_width': sa.types.String,
-            'vacancy_constructor_template_bottom_picture_path': sa.types.String,
-            'vacancy_constructor_template_bottom_picture_blurred_path': sa.types.String,
+            'languages': sa.types.JSON,
             'tech_change_date': sa.types.TIMESTAMP
-       }
+        }
     )
 
 
 #%%
 def main():
 
-    start_date = dt.datetime.today() - dt.timedelta(hours=30)
-    end_date = dt.datetime.today() - dt.timedelta(days=1)
+    start_date = dt.datetime.today() - dt.timedelta(days=30)
+    end_date = dt.datetime.today() - dt.timedelta(days=27)
 
     
     # Получаем список подходящих под наши критерии вакансий
@@ -286,6 +236,7 @@ def main():
     # Создаем датафрейм с детальным описанием
     df_with_vac = get_vacancies(id_list)
     # Загружаем данные в базу
+    df_with_vac.to_csv(f'{start_date}_{end_date}.csv')
     send_data(df_with_vac)
 
 
